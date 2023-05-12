3143b5dc6dd424c9cd66d0caa3eeefe05d4e5f8f
Anastasia Konik <konik.ftl@mail.ru>
2023-05-12 15:50:43 +0300

    feat: adding logo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <img src="src/assets/img/logo.png" alt="logo" class="center mt-3">
     <h1 class="mb-3 mt-3" style="text-align: center; font-family: 'Bold Italic Art',serif;">Experience The Power Of Knowledge</h1>
     <h2 class="mb-3 mt-3" style="text-align: center; width: 50%; margin-left: 300px; font-family: 'Rage Italic',serif">Forcecodes helps you to improve programming skills and compete with other people</h2>
     <div style="text-align: center">
